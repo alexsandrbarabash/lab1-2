8728b608c1817a8001a050c3a8592677740dfe95
Alexander <sbarabas176@gmail.com>
2022-04-27 22:27:34 +0300

    added event

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -1,16 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using queue;
+using System;
 
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new queue.Queue<int>();
+            var main = new MainService();
+            example.Added += main.OnAdded;
+            example.Add(3);
+            example.Add(2);
+            example.Add(1);
+            Console.WriteLine(example.GetElement());
+            Console.WriteLine(example.GetElement());
+            Console.WriteLine(example.GetElement());
+            Console.WriteLine(example.GetElement());
+        }
+    }
 
-var example = new queue.Queue<int>();
-
-example.Add(3);
-example.Add(2);
-example.Add(1);
-
-
-Console.WriteLine(example.GetElement());
-Console.WriteLine(example.GetElement());
-Console.WriteLine(example.GetElement());
-Console.WriteLine(example.GetElement());
+    public class MainService
+    {
+        public void OnAdded(int value)
+        {
+            Console.WriteLine("Add");
+            Console.WriteLine(value);
+        }
+    }
+}
